6fe743fedadb9e893125b8cf5cf75d5808cc448d
yume190 <yume190@gmail.com>
2015-05-25 14:51:32 +0800

    Initial Commit

---
+++ ThemeManagerVer2Tests/ThemeManagerVer2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ThemeManagerVer2Tests.swift
+//  ThemeManagerVer2Tests
+//
+//  Created by Yume on 2015/5/25.
+//  Copyright (c) 2015年 Yume. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ThemeManagerVer2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
